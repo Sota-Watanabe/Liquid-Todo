5d2850d730270ff325ae7a32547572f20aad9d9b
Jari Voutilainen <jari.voutilainen@iki.fi>
2015-10-15 15:57:44 +0300

    templates

---
+++ main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by jari-pekka.voutilain on 15/10/2015.
+ */
+
+module.exports.addTodo = function(){
+
+    var element = document.createElement('li')
+
+    document.getElementById('todo-list');
+};
