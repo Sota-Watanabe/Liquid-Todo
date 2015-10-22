c0d5aae49e57f6bcbe372cd123aac28ddd44d29f
voutilai <jari-pekka.voutilainen@tut.fi>
2015-10-22 15:17:42 +0300

    syncing to single clients

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -4727,14 +4727,93 @@ module.exports = Array.isArray || function (arr) {
 },{}],34:[function(require,module,exports){
 var VDom = require('./lib/VDom.js');
 
+var vdom;
+var opts;
 
 var Liquid = function(options){
-    this.options = options || {};
+    this.options = opts =  options || {};
     this.options.vdom = this.options.vdom || {};
-    this.vdom = new VDom(this.options.vdom);
+    this.vdom = vdom = new VDom(this.options.vdom);
+
+    this.options.createUI = this.options.createUI || true;
+    if ( this.options.createUI == true ){
+        CreateUIElements();
+    }
+
+    if (vdom.options.transferMethod == 'socket.io'){
+        vdom.client.on('list', function(data){
+            if ( opts.createUI == true ){
+                addClientsToUI(data)
+            }
+        });
+    }
 
 };
 
+
+var CreateUIElements = function(){
+
+    var container = document.createElement('div');
+    container.style.position = 'absolute';
+    container.style.bottom = 0;
+    container.style.left = 0;
+
+    var syncButton = document.createElement('button');
+    syncButton.onclick = sync;
+    syncButton.textContent = 'Sync to All';
+
+    var clientList = document.createElement('select');
+    clientList.id = 'client-list';
+    clientList.onclick = getList;
+
+    var defaultOption = document.createElement('option');
+    defaultOption.textContent = "Click to fetch clients";
+    defaultOption.disabled = 'disabled';
+    clientList.appendChild(defaultOption);
+
+    var syncToClientButton = document.createElement('button');
+    syncToClientButton.onclick = syncToClient;
+    syncToClientButton.textContent = "Sync to Client";
+
+    container.appendChild(syncButton);
+    container.appendChild(clientList);
+    container.appendChild(syncToClientButton);
+    document.body.appendChild(container);
+};
+
+var sync = function(){
+    vdom.virtualize();
+    vdom.transfer();
+};
+
+var getList = function(){
+    if (vdom.options.transferMethod == 'socket.io'){
+        vdom.client.emit('getList');
+    }
+};
+
+var addClientsToUI = function(list){
+    var select = document.querySelector('#client-list');
+
+    while (select.firstChild) {
+        select.removeChild(select.firstChild);
+    }
+
+    list.forEach(function(id){
+        var option = document.createElement('option');
+        option.value = id;
+        option.textContent = id;
+        select.appendChild(option);
+    })
+};
+
+var syncToClient = function(){
+    var select = document.querySelector('#client-list');
+    if ( select.value != "" ){
+        vdom.virtualize();
+        vdom.transferToClient(select.value);
+    }
+};
 
 module.exports = Liquid;
 
@@ -4750,10 +4829,15 @@ var initialState = null;
 
 var VDom = function(options){
     this.options = options || {};
-    if (!this.options.transferMethod){
+    if (!this.options.transferMethod) {
         this.options.transferMethod = 'socket.io';
+        this.options.path = this.options.path || '';
+    }
+
+    if ( this.options.transferMethod == 'socket.io'){
+
         var io = require('socket.io-client');
-        this.client = io('http://localhost');
+        this.client = io(this.options.path);
 
         this.client.on("sync", function(data){
             var serialized = Serializer.deserialize(data);
@@ -4785,8 +4869,10 @@ VDom.prototype.restore = function(){
 VDom.prototype.transfer = function(){
     var patches = diff(initialState, virtualizedState);
     var toSync = Serializer.serialize(patches);
-    if (this.options.transferMethod == 'socket.io'){}
-    this.client.emit("sync", toSync);
+    if (this.options.transferMethod == 'socket.io'){
+        this.client.emit("sync", toSync);
+    }
+
 };
 
 
@@ -4795,6 +4881,19 @@ VDom.prototype.restore = function(){
         var patches = diff(initialState, virtualizedState);
         patch(document.body, patches)
     }
+};
+
+VDom.prototype.transferToClient = function(client_id){
+    var patches = diff(initialState, virtualizedState);
+    var toSync = Serializer.serialize(patches);
+    var clientObj = {
+        client: client_id,
+        sync: toSync
+    };
+    if (this.options.transferMethod == 'socket.io'){
+        this.client.emit("syncToClient", clientObj);
+    }
+
 };
 
 
@@ -21041,7 +21140,12 @@ function isArray(obj) {
 },{}],"todo":[function(require,module,exports){
 var Liquid = require('liquid.js');
 
-var liquid = new Liquid();
+var liquid = new Liquid({
+    vdom: {
+        transferMethod: 'socket.io',
+        path: ''
+    }
+});
 
 module.exports.addTodo = function(text){
 
